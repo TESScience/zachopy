840901868ff55ac024483a70eb64997b36a60ecf
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-03 19:11:23 -0400

    Fixing format of dependency_links

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 # See PEP 0440: https://www.python.org/dev/peps/pep-0440/#version-scheme
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 setup(
     name='zachopy',
@@ -16,6 +16,6 @@ setup(
     author_email='zkbt@mit.edu',
     url='https://github.com/TESScience/zachopy',
     install_requires=['numpy', 'astropy', 'astroquery', 'matplotlib', 'scipy', 'pyds9==1.8.1', 'colormath', 'parse'],
-    dependency_links=['git+https://github.com/ericmandel/pyds9.git@v1.8.1#egg=pyds9-1.8.1']
+    dependency_links=['git+https://github.com/ericmandel/pyds9.git@v1.8.1#egg=pyds9-1.8.1'],
     download_url = 'https://github.com/TESScience/zachopy/tarball/{}'.format(VERSION),
 )
