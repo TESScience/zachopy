7bf7600131f0c34970ba8adfe26de065e5c0f13f
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-03 19:04:02 -0400

    Attempting to fix pyds9 problems

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
 # See PEP 0440: https://www.python.org/dev/peps/pep-0440/#version-scheme
-# Consider making this the same as the last tagged release version
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 setup(
     name='zachopy',
@@ -15,9 +14,8 @@ setup(
     description="Various tools used in lots of code written by Zach Berta-Thompson (zkbt@mit.edu)",
     author='Zach Berta-Thompson',
     author_email='zkbt@mit.edu',
-    url='https://github.com/zkbt/zachopy',
+    url='https://github.com/TESScience/zachopy',
     install_requires=['numpy', 'astropy', 'astroquery', 'matplotlib', 'scipy', 'pyds9==1.8.1', 'colormath', 'parse'],
-    dependency_links=['git+https://github.com/ericmandel/pyds9.git#egg=pyds9-1.8.1']
-    # Uncomment this if there's a tagged release that's the same as VERSION
-    # download_url = 'https://github.com/zkbt/zachopy/tarball/{}'.format(VERSION),
+    dependency_links=['git+https://github.com/ericmandel/pyds9.git@v1.8.1#egg=pyds9-1.8.1']
+    download_url = 'https://github.com/TESScience/zachopy/tarball/{}'.format(VERSION),
 )
