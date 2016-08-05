9220e9a41e404b01a4f4e3c846eede785979b178
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-04 22:28:39 -0400

    Getting rid of dependency_links now that pyds9 is on pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 # See PEP 0440: https://www.python.org/dev/peps/pep-0440/#version-scheme
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 setup(
     name='zachopy',
@@ -15,7 +15,6 @@ setup(
     author='Zach Berta-Thompson',
     author_email='zkbt@mit.edu',
     url='https://github.com/TESScience/zachopy',
-    install_requires=['numpy', 'astropy', 'astroquery', 'matplotlib', 'scipy', 'pyds9', 'colormath', 'parse'],
-    dependency_links=['git+https://github.com/ericmandel/pyds9.git@v1.8.1#egg=pyds9'],
+    install_requires=['numpy', 'astropy', 'astroquery', 'matplotlib', 'scipy', 'pyds9==1.8.1', 'colormath', 'parse'],
     download_url = 'https://github.com/TESScience/zachopy/tarball/{}'.format(VERSION),
 )
